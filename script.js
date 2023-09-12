ae38de2a12a6276470d8b8c2e46dbcd4849638b0
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-09-12 22:06:11 +0100

    added files

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-function formContainer(event){
-    event.preventDefault();
+function addData(e) {
+    
+        let userName = document.getElementById("userName");
+        let email = document.getElementById("email");
+        let password = document.getElementById("password");
 
-    let full_Name = document.getElementById("fullName");
-    let last_Name = document.getElementById("lastName");
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
+        let users = {
+            userName: userName,
+            email: email,
+            password: password,
+        }
+
+        e.preventDefault();
+        alert(getItem('users', JSON.stringify(users)))
+        
 }
+
+
+ // Check if username already exists in localStorage
+//  if (localStorage.getItem(user_Name)) {
+//     alert("Username already exists. Please choose a different one.");
+// } else {
+//     // Store username and password in localStorage
+//     localStorage.setItem(user_Name, password);
+//     alert("Signup successful! You can now login.");
+// }
+
+// // Clear signup form
+// signupForm.reset();
