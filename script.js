0e5e60a29a33e086b883bc094b18022a3498a6bb
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-09-12 20:29:29 +0100

    added files

---
+++ script.js
@@ -0,0 +1,8 @@
+function formContainer(event){
+    event.preventDefault();
+
+    let full_Name = document.getElementById("fullName");
+    let last_Name = document.getElementById("lastName");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+}
